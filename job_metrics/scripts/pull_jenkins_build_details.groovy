13b9140054ae78717cf2ef099cc7f1be2f62182b
subhatanay1234 <itsme.subhajitghosh@gmail.com>
2021-12-28 00:22:38 +0530

    Update pull_jenkins_build_details.groovy

--- job_metrics/scripts/pull_jenkins_build_details.groovy
+++ job_metrics/scripts/pull_jenkins_build_details.groovy
@@ -1,12 +1,11 @@
 
 
 
-
-
-def fetchJenkinsJobDetails(String url , String jobName,String jobId) {
+def constructJobDetailsAPIUrl(String url , String jobName,String jobId) {
     println("=================================")
     println("Loading Job Details for Job Name : " + jobName + " with Job Id " + jobId)
     def jenkins_url = "${url}/job/${jobName}/job/${jobId}/api/json"
+    println(jenkins_url)
     println("=================================")
 }
 
